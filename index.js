75d8ca75ff78672e14eae283865cc74a6f18edc3
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2021-02-11 03:42:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names, surprise){
+  names.forEach(funtion(name){
+    return  `'Thank you, ${name}, for the wonderful ${surprise} gift!'`
+  })
+}
+
+
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
