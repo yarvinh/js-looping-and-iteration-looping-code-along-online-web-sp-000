52589081f13660b27acec8a97a2210ba796c586a
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2021-02-11 03:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, surprise){
-  names.forEach(funtion(name){
+  names.map(funtion(name){
     return  `'Thank you, ${name}, for the wonderful ${surprise} gift!'`
   })
 }
