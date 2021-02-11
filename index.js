dbbb83a2b17ba5f3cba9856551cd78fcdd9f4532
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2021-02-11 04:08:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, surprise){
+return names.map(name =>  `Thank you, ${name}, for the wonderful ${surprise} gift!`)
+}
+
+
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+
+
+
+
+function countDown(countup) {
+    while (countup >= 0) {
+      console.log(countup--);
+    }
+  }
+
+  countDown(10)
